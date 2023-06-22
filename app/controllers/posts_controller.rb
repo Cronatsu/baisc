d9793bd5110fd99da0b6c30df650b3a8fb82fb93
aurr20fficial <aurr20fficial@gmail.com>
2023-06-21 22:09:03 -0700

    save all files

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def create
+    new_post = Post.create(content: params[:content])
+    render json: { post: new_post }
+  end
+  def get
+    all_posts = Post.all
+    render json: { post: all_posts }
+  end
+end
